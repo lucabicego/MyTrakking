df8c2940e7c61bd4c4514a267e7a3fa19c13a865
lucabicego <luca639.bicego@gmail.com>
2017-09-23 17:14:54 +0200

    Aggiunti percorsi

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/****************************************************************************
+ * Luca BICEGO
+ * Copyright (c) 2017
+ *
+ * via San Giorgio, 46
+ * 36030 Caltrano (VI) ITALY
+ *
+ * All rights reserved.
+ *
+ * NOTE: This sofware module can be reused / distributed with written 
+ * authorisation of the copyright holder
+ *
+ ****************************************************************************/
 var path = require("path");
 var express = require("express");
 var logger = require("morgan");
@@ -7,36 +20,18 @@ var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to l
 var app = express();
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-var entries = [];
-app.locals.entries = entries;
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/css', express.static(path.join(process.cwd() + '/public/css'))); // redirect CSS bootstrap
+//app.use('/css', express.static(path.join(process.cwd() + '/public/css'))); // redirect CSS bootstrap
 app.use('/images', express.static(path.join(process.cwd() + '/public/images'))); // redirect images
-app.use('/js', express.static(path.join(process.cwd() + '/public/js'))); // redirect js
-//app.use(express.static(path.join(__dirname, 'public')));
-app.get("/", function(request, response) 
-   {
+app.use('/js', express.static(path.join(process.cwd() + '/node_modules/bootstrap/dist/js'))); // redirect bootstrap JS
+app.use('/js', express.static(path.join(process.cwd() + '/node_modules/jquery/dist'))); // redirect JS jQuery
+app.use('/js', express.static(path.join(process.cwd() + '/node_modules/popper/dist'))); // redirect JS popper
+app.use('/css', express.static(path.join(process.cwd() + '/node_modules/bootstrap/dist/css'))); // redirect CSS bootstrap
+app.get("/", function(request, response){
       response.render("index");
    });
-/*   
-app.get("/new-entry", function(request, response)
-  {
-    response.render("new-entry");
-  });   
-app.post("/new-entry", function(request, response) 
-  {
-     if (!request.body.title || !request.body.body) 
-     {
-        response.status(400).send("Entries must have a title and a body.");
-        return;
-     }
-     entries.push({title: request.body.title,body: request.body.body,published: new Date()});
-     response.redirect("/");
-  });
-*/
-app.use(function(request, response) 
-  {
+app.use(function(request, response){
 	  response.status(404).render("404");
   });
 var listener = app.listen(port, address, function(){
