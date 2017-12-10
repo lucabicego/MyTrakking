ff8e7884bd0743baca19f5756740a03c184c944b
lucabicego <luca639.bicego@gmail.com>
2017-12-10 17:12:39 +0100

    Ripristinata gestione Cors

--- public/mymodules/webServer.js
+++ public/mymodules/webServer.js
@@ -99,15 +99,15 @@ function initWebServer()
 webServer.prototype.initRouting = function() 
 {
     //========= PAGES ===================
-    /*
+	//Per CORS
     app.all('/*', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
     })
-	*/
     //Pagina principale 
-    app.get("/", function(request, response){
+    app.get("/", function(request, response)
+	{
 	   if(request.cookies.translation == undefined)
 	   {
           //Crea un Cookie con la lingua italiana		 
